396243eedca23a4cf832bb5c6d86492cb28610a6
huafeng <1246801578@qq.com>
2020-02-12 12:39:05 +0800

    git init

---
+++ src/main/java/top/huafeng/springboot/questionnaireoline/QuestionnaireolineApplication.java
@@ -0,0 +1,13 @@
+package top.huafeng.springboot.questionnaireoline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionnaireolineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionnaireolineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/huafeng/springboot/questionnaireoline/QuestionnaireolineApplicationTests.java
@@ -0,0 +1,13 @@
+package top.huafeng.springboot.questionnaireoline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionnaireolineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
